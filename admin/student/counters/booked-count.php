0d30631f069eda4565d77d6f01ffa13fe29df265
chhavi <144299947+raif-1488@users.noreply.github.com>
2025-03-29 06:50:31 -0400

    Delete admin/student directory

--- admin/student/counters/booked-count.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../includes/dbconn.php';
-
-    $sql = "SELECT id FROM registration";
-                $query = $mysqli->query($sql);
-                echo "$query->num_rows";
-?>
--- admin/student/counters/student-count.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../includes/dbconn.php';
-
-    $sql = "SELECT id FROM userregistration";
-                $query = $mysqli->query($sql);
-                echo "$query->num_rows";
-?>
